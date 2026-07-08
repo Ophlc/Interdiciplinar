d05e87f23a5a7b93e52394329bb02ac4f6dada79
OlocoPh <mauriciofundoverde@gmail.com>
2026-07-08 17:31:29 -0300

    Pedro W

---
+++ Pedro_Wilians/scriptpedrow.js
@@ -0,0 +1,20 @@
+const musica = document.getElementById("musica");
+const botao = document.getElementById("btnMusica");
+
+botao.addEventListener("click", function () {
+
+    if (musica.paused) {
+
+        musica.play();
+
+        botao.innerHTML = "🔇 Parar Música";
+
+    } else {
+
+        musica.pause();
+
+        botao.innerHTML = "🎵 Tocar Música";
+
+    }
+
+});
